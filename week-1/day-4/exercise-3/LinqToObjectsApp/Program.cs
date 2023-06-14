dfb8aaa486f48a22e505ffabd1d733c8c6ee5d62
Saurabh Autade <saurabhautade99@gmail.com>
2023-06-14 15:58:34 +0530

    LINQObject

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -7,18 +7,34 @@
             List<Person> people = new List<Person>
             {
                 new Person { Name = "John", Age = 25, Country = "USA" },
-                new Person { Name = "Jane", Age = 30, Country = "Canada" },
+                new Person { Name = "Jane", Age = 31, Country = "Canada" },
                 new Person { Name = "Mark", Age = 28, Country = "USA" },
                 new Person { Name = "Emily", Age = 22, Country = "Australia" }
             };
-
-
-            //Write queries using LINQ for following operations
-            //1. Get all people from USA
-            //2. Get all people above 30
-            //3. Sort people by name
-            //4. Project/Select only Name and Country of all people
-
+            var USA = people.Where(P => P.Country == "USA");
+            Console.WriteLine("People from USA....");
+            foreach (var person in USA)
+            {
+                Console.WriteLine(person.Name);
+            }
+            var age = people.Where(p => p.Age >= 30);
+            Console.WriteLine("People with age above 30.....");
+            foreach (var person in age)
+            {
+                Console.WriteLine(person.Name);
+            }
+            var name = people.OrderBy(p => p.Name);
+            Console.WriteLine("Sorting by Name.....");
+            foreach (var person in name)
+            {
+                    Console.WriteLine(person.Name);
+            }
+            Console.WriteLine("Name and Country of all people....");
+            var project = people.Select(p => new { Name = p.Name, Country = p.Country }).ToList();
+            foreach (var item in project)
+            {
+                Console.WriteLine($"Name: {item.Name}, Country: {item.Country}");
+            }
         }
     }
 
