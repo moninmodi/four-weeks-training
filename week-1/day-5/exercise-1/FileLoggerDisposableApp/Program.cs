810fb7d7837fe268aece7b0e2b778992a360f529
Saurabh Autade <saurabhautade99@gmail.com>
2023-06-16 11:24:35 +0530

    FileDisposser

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            using(FileLogger logger = new FileLogger("logs.txt"))
+            {
+                logger.Log("hELLO 1");
+                logger.Log("hELLO 2");
+                logger.Log("hELLO 3");
+                logger.Log("hELLO 4");
+            }
         }
     }
 
@@ -15,16 +22,33 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath);
+
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                if( _writer != null )
+                {
+                    _writer.Flush();
+                    _writer.Close();
+                    _writer.Dispose();
+                }
+            }
         }
 
         public void Log(string message)
         {
             // Write message to the file
+            _writer.WriteLine(message);
         }
     }
 }
