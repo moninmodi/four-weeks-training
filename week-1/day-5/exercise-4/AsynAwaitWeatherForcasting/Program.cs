e5c344c14a550058d51cef2352db62ab3ff506e3
Saurabh Autade <saurabhautade99@gmail.com>
2023-06-16 14:59:35 +0530

    WeatherForecast

--- week-1/day-5/exercise-4/AsynAwaitWeatherForcasting/Program.cs
+++ week-1/day-5/exercise-4/AsynAwaitWeatherForcasting/Program.cs
@@ -1,12 +1,27 @@
-﻿namespace AsynAwaitWeatherForcasting
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AsynAwaitWeatherForcasting
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string city = "Vadodara, India";
+            string apiKey = "f058c35ac23b0300860f12ef099b94f4";
+            string city1 = "Jamnagar, India";
             // Call the method to fetch weather data
             // Display the weather data with city name
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+            string url1 = $"https://api.openweathermap.org/data/2.5/weather?q={city1}&appid={apiKey}";
+            String JSON = await FetchWeatherDataAsync(url);
+            if(!string.IsNullOrEmpty(JSON) )
+            {
+
+                WeatherData wd = JsonSerializer.Deserialize<WeatherData>(JSON);
+            }
         }
 
         // Call OpenWeatherMap API to fetch weather data https://openweathermap.org/api
@@ -15,7 +30,29 @@
         static async Task<string> FetchWeatherDataAsync(string url)
         {
             // Fetch web page content asynchronously using HttpClient
-            throw new NotImplementedException();
+            using(HttpClient client = new HttpClient())
+            {
+                string response;
+                try
+                {
+                    HttpResponseMessage message = await client.GetAsync(url);
+                    if (message.IsSuccessStatusCode)
+                    {
+                        response = await message.Content.ReadAsStringAsync();
+                        return response;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                return String.Empty;  
+            }
+        }
+        public class WeatherData
+        {
+            public string Name { get; set; }
+            public MainData Main { get; set; }
         }
     }
 }
